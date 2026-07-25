da0e169702aac6a888cdf074802a70d8c93411c6
Noorjot singh <noorjotbhullar2007@gmail.com>
2026-07-25 14:42:47 +0530

    Connected to Aiven cloud MySQL, ready for deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'o1o2o3o4noor',
-    database: 'property_app'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+    rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
